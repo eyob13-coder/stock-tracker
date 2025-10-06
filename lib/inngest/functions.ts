e72dd5198a1731d7b07dc67c7e3dbae5b14731d8
eyob13-coder <eyobgeremew618@gmail.com>
2025-10-06 16:18:24 +0300

    Implement auth logicq

--- lib/inngest/functions.ts
+++ lib/inngest/functions.ts
@@ -3,7 +3,7 @@ import {NEWS_SUMMARY_EMAIL_PROMPT, PERSONALIZED_WELCOME_EMAIL_PROMPT} from "@/li
 import {sendNewsSummaryEmail, sendWelcomeEmail} from "@/lib/nodemailer";
 import {getAllUsersForNewsEmail} from "@/lib/actions/user.actions";
 import { getWatchlistSymbolsByEmail } from "@/lib/actions/watchlist.actions";
-import { getNews } from "@/lib/actions/finhub.actions";
+import { getNews } from "@/lib/actions/finnhub.actions";
 import { getFormattedTodayDate } from "@/lib/utils";
 
 export const sendSignUpEmail = inngest.createFunction(
